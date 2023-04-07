c27f5833168afb410d7c0e33282a4cc84d620e54
Korbegy <63715343+Korbegy@users.noreply.github.com>
2023-04-07 04:31:18 +0200

    Update krb.js

--- krb.js
+++ krb.js
@@ -1052,12 +1052,155 @@ const delrey_URL = `https://live.f20.us/#${Slug}/${eventId}`;
 	getitafixture()
    // END OF ITA FIXTURE 
    
+
+
+   // fra league
+
+
+   
+   const API_URLfra = `https://site.api.espn.com/apis/site/v2/sports/soccer/fra.1/scoreboard?dates=${formattedDate}`;
+ 
+   
+   async function getfrafixture() {
+     const response = await fetch(`${API_URLfra}`);
+     const data = await response.json();
+     const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+     const today = new Date();
+     const currentDayOfWeek = today.getDay();
+	 const league = data.leagues;
+	 const Slug = league[0].slug;
+	 const events = data.events;
+     let matchesFound = false;
+     for (const event of events) {
+      
+           const homeTeam = event.competitions[0].competitors[0];
+           const awayTeam = event.competitions[0].competitors[1];
+           const detail = event.status.type.detail;
+		   const eventId = event.id;
+		   const eventDate = new Date(event.date);
+		   const estTimeStr = eventDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+           const eventDayOfWeek = eventDate.getDay();
+           const startTime = new Date(event.date);
+           const currentTime = new Date();
+           console.log(events);
+     const ita_URL = `https://live.f20.us/#${Slug}/${eventId}`
+ if (event.status.type.state === "pre"){
+		const container = document.querySelector('#frafixtures');
+       const teamContainer = document.createElement('div');
+          
+           teamContainer.innerHTML = `
+       <center>
+                 <table class="demo">
+                     <tbody>
+                 
+                    <!-- champ-->
+        
+              <tr onclick="window.open('${ita_URL}', '_blank')">
+              <div id='matchstate' onclick="location.href = '${ita_URL}'">
+              
+          </div>
+   
+             <td><img alt='${homeTeam.team.displayName} logo' src='${homeTeam.team.logo}' id='team1' width='15%' /></td>
+               <td width='32%'>${homeTeam.team.shortDisplayName}</td>
+               <td id='vs' width='5%'>VS</td>
+               <td width='32%'>${awayTeam.team.shortDisplayName}</td>
+               <td><img alt='${awayTeam.team.displayName} logo' src='${awayTeam.team.logo}' id='team2' width='15%'/></td>
+			   <td id='timetd' width='1%'><span id='time'>${estTimeStr}</span></td>
+           </tr>
+             
+        </tbody>
+                     
+                 </table></center>
+       `;
+       container.appendChild(teamContainer); 
+		 
+	 }
+    if (event.status.type.state === "in" || (event.status.type.description === "Halftime")) {
+           const container = document.querySelector('#frafixtures');
+       const teamContainer = document.createElement('div');
+          
+           teamContainer.innerHTML = `
+       <center>
+                 <table class="demo">
+                     <tbody>
+                 
+                    <!-- champ-->
+        
+              <tr onclick="window.open('${ita_URL}', '_blank')">
+              <div id='matchstate' onclick="location.href = '${ita_URL}'">
+              
+          </div>
+   
+             <td><img alt='${homeTeam.team.displayName} logo' src='${homeTeam.team.logo}' id='team1' width='15%' /></td>
+               <td width='32%'>${homeTeam.team.shortDisplayName}</td>
+               <td id='vs' width='5%'>VS</td>
+               <td width='32%'>${awayTeam.team.shortDisplayName}</td>
+               <td><img alt='${awayTeam.team.displayName} logo' src='${awayTeam.team.logo}' id='team2' width='15%'/></td>
+			   <td id='timetd' width='1%'><span id='time' class='timee' style='color:red;font-weight: 800;'> LIVE</span></td>
+           </tr>
+             
+        </tbody>
+                     
+                 </table></center>
+       `;
+       container.appendChild(teamContainer);
+      
+   }
+   // لو الماتش خلص // 
+    if (event.status.type.state === "post") {
+		const hometeamscore = event.competitions[0].competitors[0].score;
+		const awayteamscore = event.competitions[0].competitors[1].score;
+	   const container = document.querySelector('#frafixtures');
+       const teamContainer = document.createElement('div');
+          
+           teamContainer.innerHTML = `
+       <center>
+                 <table class="demo">
+                     <tbody>
+                 
+                    <!-- champ-->
+        
+              <tr>
+              <div>
+              
+          </div>
+   
+             <td><img alt='${homeTeam.team.displayName} logo' src='${homeTeam.team.logo}' id='team1' width='15%' /></td>
+               <td width='32%'>${homeTeam.team.shortDisplayName}</td>
+               <td id='vs' width='5%'>FT</td>
+               <td width='32%'>${awayTeam.team.shortDisplayName}</td>
+               <td><img alt='${awayTeam.team.displayName} logo' src='${awayTeam.team.logo}' id='team2' width='15%'/></td>
+			   <td id='timetd' width='1%'><span id='time'>
+			   
+			  <img src='${homeTeam.team.logo}' style='height:15px;'/> ${hometeamscore} : ${awayteamscore} <img src='${awayTeam.team.logo}' style='height:15px;'/>
+			   
+			   </span></td>
+           </tr>
+             
+        </tbody>
+                     
+                 </table></center>
+       `;
+       container.appendChild(teamContainer);
+      
+		
+	}
+	
+   matchesFound = true;
+    
+
+   }
+    //   IF NO MATCHES TODAY SHOW THIS CODE 
+    if (!matchesFound) {document.getElementById("frafixtures").style.display = "none";}
+   }
+	getfrafixture()
+   // END OF FRA FIXTURE 
+
+
         
 
    // FA CUP league
 
-
-   
    const API_URLfa = `https://site.api.espn.com/apis/site/v2/sports/soccer/eng.fa/scoreboard?dates=${formattedDate}`;
  
    
