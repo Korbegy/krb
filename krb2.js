2238fa3818c97b78b1d5d7222e35587805ad4337
Korbegy <63715343+Korbegy@users.noreply.github.com>
2023-08-27 13:21:37 +0300

    Update krb2.js

--- krb2.js
+++ krb2.js
@@ -746,7 +746,7 @@
                      const hometeamscore = event.competitions[0].competitors[0].score;
                      const awayteamscore = event.competitions[0].competitors[1].score;
                      console.log(league);
-               const es_URL = `https://live.f20.us/#${Slug}/${eventId}`;
+               const es_URL = `https://hesgoal.f20.us/#${Slug}/${eventId}`;
                if (event.status.type.state === "pre"){
                   const container = document.querySelector('#esfixtures');
                  const teamContainer = document.createElement('div');
