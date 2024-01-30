81055949fbb4a49b88d0e2ef549420d6e93950be
Korbegy <63715343+Korbegy@users.noreply.github.com>
2024-01-31 00:07:38 +0200

    Update saudi3.js

--- saudi3.js
+++ saudi3.js
@@ -2376,11 +2376,7 @@ $(document).ready(function () {
                                       <path stroke-width="9" stroke="currentColor" d="M44.25 36.3612L17.25 51.9497C11.5833 55.2213 4.5 51.1318 4.50001 44.5885L4.50001 13.4115C4.50001 6.86824 11.5833 2.77868 17.25 6.05033L44.25 21.6388C49.9167 24.9104 49.9167 33.0896 44.25 36.3612Z"></path>
                                   </svg> Live
                               </button>` : 
-                              `<button class="event-button-soon" onclick="handleButtonClick('${title}')">
-                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 53 58" height="15" width="15">
-                                      <path stroke-width="9" stroke="currentColor" d="M44.25 36.3612L17.25 51.9497C11.5833 55.2213 4.5 51.1318 4.50001 44.5885L4.50001 13.4115C4.50001 6.86824 11.5833 2.77868 17.25 6.05033L44.25 21.6388C49.9167 24.9104 49.9167 33.0896 44.25 36.3612Z"></path>
-                                  </svg> Soon
-                              </button>`
+                              ``
                           }
                           </div>
                       </div>
